7ae7948f6048780b8d87f2a5cd6578641fe981c1
adithiachristian7 <adithiachristian@gmail.com>
2024-11-09 13:51:45 +0800

    menambah component hero

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
 import "./App.css";
 import Header from "./components/header/index.js";
+import Hero from "./components/hero/hero.js";
 
 function App() {
   return (
     <div>
       <Header />
-      <section className="hero" id="home">
-        <div className="main-content" data-aos="none">
-          <h4>Hello,</h4>
-          <h1>
-            I'm <span>Adithia Christian</span>
-          </h1>
-          <p>I'm a Student at Klabat University and a Junior UIUX Designer</p>
-          <div className="social">
-            <a href="#">
-              <i className="ri-facebook-fill" />
-            </a>
-            <a href="#">
-              <i className="ri-instagram-fill" />
-            </a>
-            <a href="#">
-              <i className="ri-twitter-fill" />
-            </a>
-          </div>
-          <div className="main-btn">
-            <a href="#" className="btn btn2">
-              Download CV
-            </a>
-          </div>
-        </div>
-      </section>
+      <Hero />
       <section className="about" id="about">
         <div className="about-img" data-aos="zoom-in-down">
           <img src="assets/img/adit3.jpg" />
---
+++ src/components/hero/hero.js
@@ -0,0 +1,31 @@
+function Hero() {
+  return (
+    <section className="hero" id="home">
+      <div className="main-content" data-aos="none">
+        <h4>Hello,</h4>
+        <h1>
+          I'm <span>Adithia Christian</span>
+        </h1>
+        <p>I'm a Student at Klabat University and a Junior UIUX Designer</p>
+        <div className="social">
+          <a href="#">
+            <i className="ri-facebook-fill" />
+          </a>
+          <a href="#">
+            <i className="ri-instagram-fill" />
+          </a>
+          <a href="#">
+            <i className="ri-twitter-fill" />
+          </a>
+        </div>
+        <div className="main-btn">
+          <a href="#" className="btn btn2">
+            Download CV
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
